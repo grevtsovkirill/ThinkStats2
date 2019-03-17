0ea49cc9e068230048c125450c4b599f98c46d55
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2019-03-17 16:27:50 +0100

    plain py(not notebook) - df, operations with lists

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -19,8 +19,28 @@ def main(script):
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
-
+    respdf=nsfg.ReadFemResp()
+    #respdf.head()
+    pregnum=respdf['pregnum']
+    preg_stat=pregnum.value_counts().sort_index()
+    list_of_Npregs=pregnum.unique()
+    list_of_Npregs.sort()
+    preg_stat_nsfg=[]
+    print("list_of_Npregs",list_of_Npregs)
+    Npregs_7_95=0
+    Npregs_tot=0
+    for i in list_of_Npregs:
+        Npregs_tot+=preg_stat[i]
+        if i<7:
+            preg_stat_nsfg.append((i,preg_stat[i]))
+        if i>6:
+            Npregs_7_95+=preg_stat[i]
+    print("pregnums:\n","\n".join([str(x) for x in preg_stat_nsfg]),"\n7-95 ",Npregs_7_95,"\nTotal = ",Npregs_tot)
+    dict_toPreg=nsfg.MakePregMap(respdf)
+    #print("dict_toPreg",dict_toPreg)
+    
+    #print('%s: All tests passed.' % script)
+    
 
 if __name__ == '__main__':
     main(*sys.argv)
